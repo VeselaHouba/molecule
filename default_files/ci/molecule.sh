4ea83cb45356d81273cca9bc099c753cec897ec8
Michalek <michalek@m-cloud.cz>
2022-02-17 16:10:43 +0100

    Molecule docker default files

---
+++ default_files/ci/molecule.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+docker \
+  run \
+  --rm \
+  -it \
+  -v "$(pwd):/tmp/$(basename "${PWD}")" \
+  -v /var/run/docker.sock:/var/run/docker.sock \
+  -w "/tmp/$(basename "${PWD}")" \
+  -e MOLECULE_NO_LOG=false \
+  -e MOLECULE_IMAGE \
+  veselahouba/molecule bash -c "
+  shellcheck_wrapper && \
+  flake8 && \
+  yamllint . && \
+  ansible-lint && \
+  cp -a ./ /tmp/role/ && \
+  cd /tmp/role && \
+  curl https://raw.githubusercontent.com/VeselaHouba/molecule/master/molecule-docker/pull_files.sh > ci/pull_files.sh && \
+  bash ci/pull_files.sh && \
+  molecule ${*}"
